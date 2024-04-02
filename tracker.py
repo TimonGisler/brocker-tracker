fc0df914ce6bf687aae8d3f320f441ddd533622c
Your Name <timongisler@icloud.com>
2024-04-02 19:22:00 +0200

    another one, pls last one : )))

--- tracker.py
+++ tracker.py
@@ -75,6 +75,7 @@ def get_page_text_using_selenium(url_to_scrape):
     # set the language to english --> to avoid language detection by the website (regex pattern is in english)
     options.add_argument('lang=en-GB')
     options.add_argument('--disable-translate')
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'}) # https://stackoverflow.com/questions/55150118/trouble-modifying-the-language-option-in-selenium-python-bindings
 
     if os.path.exists("/.dockerenv"): # check if we are in a docker container --> if yes, there is no chrome driver installed, and I have to call the remote driver
         print("Running in docker container, using remote selenium")
